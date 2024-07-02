f8111d94b7ef1f5d1f4fadda7bf657528242d949
Pedro de Andrade <dev.pedroandrade@gmail.com>
2024-07-02 17:15:40 -0300

    feat: Documentação de tokens

--- packages/docs/src/stories/buttons.stories.tsx
+++ packages/docs/src/stories/buttons.stories.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from '@pedroandrad1/react'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'Texto',
